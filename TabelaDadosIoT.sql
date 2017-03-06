68038db79f199f50b6a3ddf364007a276adfa7f4
Diovane Soligo <diovane_soligo@hotmail.com>
2017-03-06 17:59:32 -0300

    .

--- TabelaDadosIoT.sql
+++ TabelaDadosIoT.sql
@@ -12,8 +12,24 @@ create table dados(
 	corrente float not null,
 	potencia float not null,
 	data date,
-	hota time,
+	hora time,
 	data_hora timestamp(0),
 	foreign key (id_disp) references dispositivos (id_disp)
 );
 
+
+
+select *from CURRENT_TIME(1)
+
+insert into dispositivos (id_disp,nome,estado) values (001,'Tomada Teste IoT',1);
+
+insert into dados (id_disp,voltagem,corrente,potencia)values(001,210,0.32,67.2);
+
+
+select *from dispositivos where id_disp = 001
+
+select *from dados
+
+delete from dados
+
+drop table dados
