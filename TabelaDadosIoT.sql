a4aeb0536e1b6560b2d9b96d055d35eb3b421297
Diovane Soligo <diovane_soligo@hotmail.com>
2017-03-06 10:43:55 -0300

    tabela dados IoT

---
+++ TabelaDadosIoT.sql
@@ -0,0 +1,17 @@
+﻿create table dispositivos(
+	id_disp int not null,
+	nome varchar(50),
+	primary key (id_disp)
+);
+
+create table dados(
+	id_disp int not null,
+	id_dado serial,
+	voltagem int not null,
+	corrente float not null,
+	potencia float not null,
+	data date,
+	hota time,
+	data_hora timestamp(0),
+	foreign key (id_disp) references dispositivos (id_disp)
+);
