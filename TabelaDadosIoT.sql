e5802fb1213a0c65e5e75f444323db6bdbb8f697
Diovane Soligo <diovane_soligo@hotmail.com>
2017-03-08 11:37:15 -0300

    .

--- TabelaDadosIoT.sql
+++ TabelaDadosIoT.sql
@@ -6,6 +6,7 @@
 create table dispositivos(
 	USUARIO int not null,
 	id_disp int not null, 	/* ID do dispositivo*/
+	IP_disp varchar(50),
 	nome varchar(50),	/* nome referencia tomada*/
 	estado int not null,	/* 1 ligado - 0 desligado*/
 	primary key (id_disp),
@@ -29,7 +30,7 @@ select *from usuario
 
 select *from dispositivos where usuario = 1 order by id_disp
 
-update dispositivos set estado=0 where id_disp = 4
+update dispositivos set ip_disp='192.168.90.119' where id_disp = 1
 
 
 select *from CURRENT_TIME(1)
@@ -37,12 +38,12 @@ insert into usuario(USUARIO) values(1);
 insert into usuario(USUARIO) values(2);
 insert into usuario(USUARIO) values(3);
 
-insert into dispositivos (USUARIO,id_disp,nome,estado) values (1,001,'Tomada Teste IoT',1);
-insert into dispositivos (USUARIO,id_disp,nome,estado) values (1,004,'Tomada 2 Teste IoT',1);
-insert into dispositivos (USUARIO,id_disp,nome,estado) values (1,005,'Tomada 3 Teste IoT',1);
+insert into dispositivos (USUARIO,id_disp,IP_disp,nome,estado) values (1,001,'http://192.168.90.119','Tomada Teste IoT',1);
+insert into dispositivos (USUARIO,id_disp,IP_disp,nome,estado) values (1,004,'http://','Tomada 2 Teste IoT',1);
+insert into dispositivos (USUARIO,id_disp,IP_disp,nome,estado) values (1,005,'http://','Tomada 3 Teste IoT',1);
 
-insert into dispositivos (USUARIO,id_disp,nome,estado) values (2,002,'Tomada Teste IoT 2',1);
-insert into dispositivos (USUARIO,id_disp,nome,estado) values (3,003,'Tomada Teste IoT 3',1);
+insert into dispositivos (USUARIO,id_disp,IP_disp,nome,estado) values (2,002,'http://','Tomada Teste IoT 2',1);
+insert into dispositivos (USUARIO,id_disp,IP_disp,nome,estado) values (3,003,'http://','Tomada Teste IoT 3',1);
 
 insert into dados (id_disp,voltagem,corrente,potencia)values(001,210,0.32,67.2);
 
@@ -53,4 +54,7 @@ select *from dados
 
 select *from dispositivos
 
+
+drop table dados
 drop table dispositivos
+drop table usuario
